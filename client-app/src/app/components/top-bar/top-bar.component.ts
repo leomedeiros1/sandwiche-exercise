bed3a25b2c3bc7fc0b366ff16312cd563b886c6d
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2024-04-26 02:07:36 -0300

    Improve top-bar layout

--- client-app/src/app/components/top-bar/top-bar.component.ts
+++ client-app/src/app/components/top-bar/top-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
